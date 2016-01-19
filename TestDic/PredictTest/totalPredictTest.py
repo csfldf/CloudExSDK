76ef97b3fd527ed78eb558e7be690d804e8839ff
sk <234815790@qq.com>
2016-01-19 14:31:30 +0800

    小修改

--- TestDic/PredictTest/totalPredictTest.py
+++ TestDic/PredictTest/totalPredictTest.py
@@ -23,7 +23,7 @@ f.close()
 mau = MAPredictUtil()
 acrcu = ACRCPredictUtil()
 dailyu = DailyPredictUtil()
-PeriodicPredictUtil = PeriodicPredictUtil()
+periodicu = PeriodicPredictUtil()
 
 maPredictList = []
 periodicPredictList = []
@@ -37,8 +37,8 @@ for wl in realWL:
     addWLToDailyWindow(wl)
 
     maPredictWL = mau.getNextPeriodWorkload()
-    periodicPredictWL = PeriodicPredictUtil.getNextPeriodWorkload()
-    dailyPreidictWL = DailyPredictUtil.getNextPeriodWorkload()
+    periodicPredictWL = periodicu.getNextPeriodWorkload()
+    dailyPreidictWL = dailyu.getNextPeriodWorkload()
 
     addPWLToMAWindow(maPredictWL)
     addPWLToPeriodicWindow(periodicPredictWL)
@@ -60,7 +60,7 @@ periodicPredictList.pop()
 
 maCompareTuple = zip(realWL, maPredictList)
 dailyCompareTuple = zip(realWL, dailyPredictList)
-periodicCompareTuple = zip(realWL, periodicPredictWL)
+periodicCompareTuple = zip(realWL, periodicPredictList)
 #statPD.pop(0)
 #statPD.pop()
 
@@ -99,51 +99,151 @@ periodicSheet.write_column('A2', index)
 periodicSheet.write_column('B2', realWL)
 periodicSheet.write_column('C2', maPredictList)
 
-chart = workbook.add_chart({'type': 'scatter',
+maChart = workbook.add_chart({'type': 'scatter',
                                  'subtype': 'smooth_with_markers'})
-chart.add_series({
-        'name': '=Sheet1!$B$1',
-        'categories': '=Sheet1!$A$2:$A$' + str(1 + len(index)),
-        'values': '=Sheet1!$B$2:$B$' + str(1 + len(index))})
+maChart.add_series({
+        'name': '=ma!$B$1',
+        'categories': '=ma!$A$2:$A$' + str(1 + len(index)),
+        'values': '=ma!$B$2:$B$' + str(1 + len(index))})
 
-chart.add_series({
-        'name': '=Sheet1!$C$1',
-        'categories': '=Sheet1!$A$2:$A$' + str(1 + len(index)),
-        'values': '=Sheet1!$C$2:$C$' + str(1 + len(index))})
-
-
+maChart.add_series({
+        'name': '=ma!$C$1',
+        'categories': '=ma!$A$2:$A$' + str(1 + len(index)),
+        'values': '=ma!$C$2:$C$' + str(1 + len(index))})
 
 maRelativeError = calculateRelativeError(maCompareTuple)
-maQualified = calculatemaQualified(maCompareTuple)
+maQualified = calculateQualified(maCompareTuple)
 
 maRelativeError = round(maRelativeError * 100, 2)
 maQualified = round(maQualified * 100, 2)
 
+print 'ma: RelativeError: %f, Qualified: %f' % (maRelativeError, maQualified)
+
+maChart.set_size({'width': 1400, 'height': 800})
+
+maChart.set_title({'name': 'ma RelativeError:' + str(maRelativeError) + '% PassRate:' + str(maQualified) + '%'})
+maSheet.insert_chart('E1', maChart)
+
+
+periodicChart = workbook.add_chart({'type': 'scatter',
+                                 'subtype': 'smooth_with_markers'})
+periodicChart.add_series({
+        'name': '=periodic!$B$1',
+        'categories': '=periodic!$A$2:$A$' + str(1 + len(index)),
+        'values': '=periodic!$B$2:$B$' + str(1 + len(index))})
+
+periodicChart.add_series({
+        'name': '=periodic!$C$1',
+        'categories': '=periodic!$A$2:$A$' + str(1 + len(index)),
+        'values': '=periodic!$C$2:$C$' + str(1 + len(index))})
+
+
 periodicRelativeError = calculateRelativeError(periodicCompareTuple)
-periodicQualified = calculateperiodicQualified(periodicCompareTuple)
+periodicQualified = calculateQualified(periodicCompareTuple)
 
 periodicRelativeError = round(periodicRelativeError * 100, 2)
 periodicQualified = round(periodicQualified * 100, 2)
 
+print 'periodic: RelativeError: %f, Qualified: %f' % (periodicRelativeError, periodicQualified)
+
+periodicChart.set_size({'width': 1400, 'height': 800})
+
+periodicChart.set_title({'name': 'periodic RelativeError:' + str(periodicRelativeError) + '% PassRate:' + str(periodicQualified) + '%'})
+periodicSheet.insert_chart('E1', periodicChart)
+
+
+
+dailyChart = workbook.add_chart({'type': 'scatter',
+                                 'subtype': 'smooth_with_markers'})
+dailyChart.add_series({
+        'name': '=daily!$B$1',
+        'categories': '=daily!$A$2:$A$' + str(1 + len(index)),
+        'values': '=daily!$B$2:$B$' + str(1 + len(index))})
+
+dailyChart.add_series({
+        'name': '=daily!$C$1',
+        'categories': '=daily!$A$2:$A$' + str(1 + len(index)),
+        'values': '=daily!$C$2:$C$' + str(1 + len(index))})
 
 dailyRelativeError = calculateRelativeError(dailyCompareTuple)
-dailyQualified = calculatedailyQualified(dailyCompareTuple)
+dailyQualified = calculateQualified(dailyCompareTuple)
 
 dailyRelativeError = round(dailyRelativeError * 100, 2)
 dailyQualified = round(dailyQualified * 100, 2)
 
+print 'daily: RelativeError: %f, Qualified: %f' % (dailyRelativeError, dailyQualified)
 
-chart.set_size({'width': 1400, 'height': 800})
+dailyChart.set_size({'width': 1400, 'height': 800})
 
-chart.set_title({'name': 'ma RelativeError:' + str(maRelativeError) + '% PassRate:' + str(maQualified) + '%'})
-maSheet.insert_chart('E1', chart)
 
-chart.set_title({'name': 'periodic RelativeError:' + str(periodicRelativeError) + '% PassRate:' + str(periodicQualified) + '%'})
-periodicSheet.insert_chart('E1', chart)
+dailyChart.set_title({'name': 'daily RelativeError:' + str(dailyRelativeError) + '% PassRate:' + str(dailyQualified) + '%'})
+dailySheet.insert_chart('E1', dailyChart)
 
-chart.set_title({'name': 'daily RelativeError:' + str(dailyRelativeError) + '% PassRate:' + str(dailyQualified) + '%'})
-dailySheet.insert_chart('E1', chart)
 
+#acrc
+clearAllData()
+
+f = open('../DataFile/workload.txt')
+realWL = []
+for line in f:
+    l = re.split(r'\s+', line)
+    realWL.append(int(l[1]))
+f.close()
+
+acrcu = ACRCPredictUtil()
+acrcPredictList = []
+
+for wl in realWL:
+    #acrc
+    addWLToACRCWindow(wl)
+    acrcPredictWL = acrcu.getNextPeriodWorkload()
+    addPWLToACRCWindow(acrcPredictWL)
+    acrcPredictList.append(acrcPredictWL)
+
+
+realWL.pop(0)
+acrcPredictList.pop()
+
+acrcCompareTuple = zip(realWL, acrcPredictList)
+
+index = range(len(realWL))
+
+
+acrcSheet = workbook.add_worksheet('acrc')
+
+bold = workbook.add_format({'bold': 1})
+
+headings = ['index', 'Real', 'Predicted']
+
+acrcSheet.write_row('A1', headings, bold)
+acrcSheet.write_column('A2', index)
+acrcSheet.write_column('B2', realWL)
+acrcSheet.write_column('C2', acrcPredictList)
+
+
+acrcChart = workbook.add_chart({'type': 'scatter',
+                                 'subtype': 'smooth_with_markers'})
+acrcChart.add_series({
+        'name': '=acrc!$B$1',
+        'categories': '=acrc!$A$2:$A$' + str(1 + len(index)),
+        'values': '=acrc!$B$2:$B$' + str(1 + len(index))})
+
+acrcChart.add_series({
+        'name': '=acrc!$C$1',
+        'categories': '=acrc!$A$2:$A$' + str(1 + len(index)),
+        'values': '=acrc!$C$2:$C$' + str(1 + len(index))})
+
+acrcRelativeError = calculateRelativeError(acrcCompareTuple)
+acrcQualified = calculateQualified(acrcCompareTuple)
+
+acrcRelativeError = round(acrcRelativeError * 100, 2)
+acrcQualified = round(acrcQualified * 100, 2)
+
+print 'acrc: RelativeError: %f, Qualified: %f' % (acrcRelativeError, acrcQualified)
+
+acrcChart.set_size({'width': 1400, 'height': 800})
+
+acrcChart.set_title({'name': 'acrc RelativeError:' + str(acrcRelativeError) + '% PassRate:' + str(acrcQualified) + '%'})
+acrcSheet.insert_chart('E1', acrcChart)
 
 workbook.close()
-
