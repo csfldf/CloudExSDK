be6d91dfcdaf99a0a5feffb60d6237edcc92efc5
sk <234815790@qq.com>
2016-01-20 22:56:08 +0800

    添加ACRCUtil

---
+++ ACRCUtil/ACRCPlacementComponent.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from ACRCUtil.PlacementComponent import PlacementComponent
+
+class ACRCPlacementComponent(object):
+
+
+
---
+++ ACRCUtil/PlacementComponent.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+class PlacementComponent(object):
+    def getPlacementScheme(self, amount, upOrDown):
+        pass
